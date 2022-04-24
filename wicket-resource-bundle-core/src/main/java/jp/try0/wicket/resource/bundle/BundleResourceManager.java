986bb80fbe6777421b4b79046aca23c98c196d45
Ryo <try0.development@gmail.com>
2022-04-24 11:07:44 +0900

    hash

--- wicket-resource-bundle-core/src/main/java/jp/try0/wicket/resource/bundle/BundleResourceManager.java
+++ wicket-resource-bundle-core/src/main/java/jp/try0/wicket/resource/bundle/BundleResourceManager.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.Stack;
 import java.util.stream.Collectors;
@@ -549,7 +550,7 @@ public class BundleResourceManager {
 
 		@Override
 		public int hashCode() {
-			return clazz.hashCode();
+			return Objects.hash(clazz);
 		}
 	}
 
